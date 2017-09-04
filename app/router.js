45beee53f195efd28e77aa698d603f50cc2b7ea0
mynameismaosheng <982431279@qq.com>
2017-09-04 22:06:54 +0800

    nav,router

--- app/router.js
+++ app/router.js
@@ -1,1 +1,7 @@
-$('#app').load('app/a.txt');
+$('#app').load('app/container.html');
+$(function(){
+    $(document).on('click','.router-nav',function(){
+        var routerName = $(this).data('router');
+        $('.right_container').load(routerName);
+    })
+})

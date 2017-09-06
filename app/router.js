f9af8b71bb7c0c5804cc82bafcdbcbc9a61cc070
maunsell <982431279@qq.com>
2017-09-06 15:14:23 +0800

    数据表格,router完善.

--- app/router.js
+++ app/router.js
@@ -1,7 +1,14 @@
-$('#app').load('app/container.html');
-$(function(){
-    $(document).on('click','.router-nav',function(){
+$(function () {
+    $('#app').load('app/container.html', function () {
+        $(document).find('.router-nav:first').click();
+    });
+    $(document).on('click', '.router-nav', function () {
         var routerName = $(this).data('router');
-        $('.right_container').load(routerName);
+        $('#content').load(routerName, function () {
+            $(document).find('.router-subnav:first').click();
+        });
+    }).on('click', '.router-subnav', function () {
+        var routerName = $(this).data('router');
+        $('#right_container').load(routerName);
     })
 })
